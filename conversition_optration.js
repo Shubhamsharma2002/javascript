194f5965efa046cd3676aedd3a65274958f430f2
Shubham Sharma <shubhamtech004@gmail.com>
2023-10-24 18:05:53 +0530

    data types and conversition

---
+++ conversition_optration.js
@@ -0,0 +1,5 @@
+let score = 55;
+// first way to print the type 
+console.log(typeof score);
+// second way to print the type
+console.log(typeof(score));
---
+++ data_type.js
@@ -0,0 +1,31 @@
+/*
+1. treat all js code as newer version
+2. but modern js not compulasory to write this..
+3. Also maintain the readability of your code ...
+*/
+"use strict ";  
+
+
+/* 
+
+########### DATA TYPES ##########
+**primitive data ****
+number => 1 to power 53
+bigint 
+string => ""
+boolean => true/false
+null => standalone value 
+undefined => 
+symbol  => unique 
+
+** non primitive **
+
+
+*/
+
+
+// !!!!!!!!!!.........NOTE...........!!!!!!!!!!!!!!!
+ 
+
+console.log(typeof undefined);   // === undefined
+ console.log(typeof null);       // == object
