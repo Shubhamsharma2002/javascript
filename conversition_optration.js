9278cd4cc5d9f66ebf9bd267b879561168316a84
Shubham Sharma <shubhamtech004@gmail.com>
2023-10-24 18:30:40 +0530

    some conversition

--- conversition_optration.js
+++ conversition_optration.js
@@ -3,3 +3,16 @@ let score = 55;
 console.log(typeof score);
 // second way to print the type
 console.log(typeof(score));
+
+/*
+"33" converted into 33
+"33abc" converted into NaN
+true  converted into 1
+false converted into 0
+
+
+1 converted into true 
+0 converted into false 
+"" return false 
+"shubhi" return true
+*/
