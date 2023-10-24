b3087c2d20d122055335e786e94f60e10048668d
Shubham Sharma <shubhamtech004@gmail.com>
2023-10-24 18:29:34 +0530

    some conversition

---
+++ c_operation.js
@@ -0,0 +1,9 @@
+let num = 90;
+let negnum = -num;
+// print negative number
+//console.log(negnum);
+
+console.log("1" + 2);  // == 12
+console.log(1+"2");  // == 12
+console.log("1" + 2 + 2); // == 122
+console.log(1 + 2 + "2"); // == 32
