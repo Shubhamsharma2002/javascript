cc7aa121d604d38d58e8c8f3b1f4d7a47494946b
Shubham Sharma <shubhamtech004@gmail.com>
2023-10-25 18:33:01 +0530

    string of js

---
+++ 8_string.js
@@ -0,0 +1,12 @@
+const name = "shubham";
+const repocount = 40;
+// new way to add string 
+console.log(`Hello guys myself ${name.toUpperCase()} and my githhub repocount is ${repocount}`);
+
+const newname = new String('shubhi');
+console.log(newname); // output [String: 'shubhi']
+
+
+
+
+// notes read the methods of strings
