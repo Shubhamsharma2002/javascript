32cdaa21153a09c6deb24de1ea324d0b090a6459
Shubham Sharma <shubhamtech004@gmail.com>
2023-10-26 11:42:07 +0530

    maths and number in js

---
+++ 9_num_maths.js
@@ -0,0 +1,34 @@
+// 1st way to decalare number
+
+const score = 500;
+console.log(score);
+
+// 2nd way to declare number
+
+const blance = new Number(200);
+console.log(blance);
+
+// convert into string
+console.log(blance.toString());
+// check the length 
+console.log(blance.toString().length);
+
+
+
+
+
+/*
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!....................maths....................
+
+*/
+
+console.log(Math);
+
+// onvert - to plus use abs function
+
+console.log(Math.abs(-4));
+
+// for round of number
+
+console.log(Math.round(4.89));
