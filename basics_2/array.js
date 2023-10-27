ee1c2cb9003039a6723acc1bb30322a18a5d4f5c
Shubham Sharma <shubhamtech004@gmail.com>
2023-10-27 19:51:19 +0530

    array and methods

---
+++ basics_2/array.js
@@ -0,0 +1,28 @@
+
+// decalare 1st way 
+let girlsname = ['anu', 'priya','anjali', 'poonam'];
+console.log(girlsname[0]);
+
+// decalare 2nd way
+
+const numarr = [1,2,3,4,5,6,7,8];
+console.log(numarr[5]);
+
+// decalare 3rd way 
+
+const arr = new Array(2,33,4,5);
+console.log(arr[1]);
+
+
+// ******************** METHODS OF ARRAY  ******************************
+
+//  1 . push method 
+numarr.push(89);
+console.log(numarr);
+//  2 . pop method 
+numarr.pop();
+console.log(numarr);
+
+
+// array .splice 
+// array is from 
